80fb4acc50a86866995b3a2d8dc78d5d533ff285
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-12 18:27:47 +0530

    Added:welcome message to user

---
+++ src/main/java/com/javaprogram/GenericMaxValue/GenericMaxValue.java
@@ -0,0 +1,8 @@
+package com.javaprogram.GenericMaxValue;
+
+public class GenericMaxValue {
+	public static void main(String[] args) {
+		System.out.println("Welcome to generics");
+	}
+
+}
---
+++ src/test/java/com/javaprogram/GenericMaxValue/GenericMaxValue.java
@@ -0,0 +1,14 @@
+package com.javaprogram.GenericMaxValue;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GenericMaxValue {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
